09e2c2ff3cb2444e04d99ef48f557e66deee3772
OldOak <artxakrus@yandex.ru>
2026-08-12 14:29:36 +0400

    Исправлены переменные времени

--- stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,10 +1,13 @@
 package ru.practicum.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -24,5 +27,6 @@ public class EndpointHitDto {
     private String ip;
 
     @NotBlank
-    private String timestamp;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
 }
--- stats/stats-server/src/main/java/ru/practicum/stats/model/EndpointHit.java
+++ stats/stats-server/src/main/java/ru/practicum/stats/model/EndpointHit.java
@@ -1,16 +1,7 @@
 package ru.practicum.stats.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -36,6 +27,6 @@ public class EndpointHit {
     @Column(name = "ip", nullable = false, length = 45)
     private String ip;
 
-    @Column(name = "timestamp", nullable = false)
+    @Column(name = "hit_timestamp", nullable = false)
     private LocalDateTime timestamp;
 }
